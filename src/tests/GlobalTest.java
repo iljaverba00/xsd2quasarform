2f5c29912f6bce72353e30d5b0643cd4b758aa25
verba <ilja-verba00@yandex.ru>
2024-08-14 17:12:34 +0800

    4.1 added tests filled forms

--- src/tests/GlobalTest.java
+++ src/tests/GlobalTest.java
@@ -8,17 +8,34 @@ public class GlobalTest {
 
     private final Converter converter = new Converter();
 
+
+    // interact-map-plan-v01-r04
+
+    @Test
+    public void testInteractPlan() {
+        String xsd = "example-interact-map-plan-v01-R04/interact_map_plan/interact_map_plan_v01.xsd";
+        String html = "example-interact-map-plan-v01-R04/form.html";
+        converter.run(xsd, html);
+    }
+
+    @Test
+    public void testInteractPlan_filledForm() {
+        String xsd = "example-interact-map-plan-v01-R04/interact_map_plan/interact_map_plan_v01.xsd";
+        String html = "example-interact-map-plan-v01-R04/filled-form.html";
+        converter.run(xsd, html);
+    }
+
     @Test
     public void testNewDesign() {
-        String xsd = "example-new-design/test.xml";
+        String xsd = "example-new-design/schema.xsd";
         String html = "example-new-design/form.html";
         converter.run(xsd, html);
     }
 
     @Test
-    public void testInteractPlan_01() {
-        String xsd = "example-interact-map-plan-v01-R04/interact_map_plan/interact_map_plan_v01.xsd";
-        String html = "example-interact-map-plan-v01-R04/form.html";
+    public void testNewDesign_filledForm() {
+        String xsd = "example-new-design/test.xml";
+        String html = "example-new-design/filled-form.html";
         converter.run(xsd, html);
     }
 }
