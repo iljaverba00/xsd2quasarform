776a33d9d5ab229995e02fe5ebfc48146259e949
verba <ilja-verba00@yandex.ru>
2024-08-14 17:55:52 +0800

    4.3 move

---
+++ src/tests/NameSpaces.java
@@ -0,0 +1,17 @@
+package tests;
+
+import com.java.Converter;
+import org.junit.Test;
+
+
+public class NameSpaces {
+
+    private final Converter converter = new Converter();
+
+    @Test
+    public void test() {
+        String xsd = "namespaces/namespaces-sample.xsd";
+        String html = "namespaces/form.html";
+        converter.run(xsd, html);
+    }
+}
