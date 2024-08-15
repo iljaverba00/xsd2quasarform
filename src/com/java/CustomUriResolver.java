b81d4843681bd7121d838b58bf0cb41a439eb799
verba <ilja-verba00@yandex.ru>
2024-08-15 16:46:41 +0800

    file test

--- src/com/java/CustomUriResolver.java
+++ src/com/java/CustomUriResolver.java
@@ -1,5 +1,7 @@
 package com.java;
 
+import sun.misc.IOUtils;
+
 import javax.xml.transform.*;
 import javax.xml.transform.stream.StreamSource;
 import java.io.ByteArrayInputStream;
@@ -14,8 +16,9 @@ import java.util.Map;
 public class CustomUriResolver implements URIResolver {
 
     private final ClassLoader loader = CustomUriResolver.class.getClassLoader();
-    public static final String basePathForXsl = "resources/";
+    public static final String basePathForXsl = "src/resources/";
     private Map<String, byte[]> cache = new HashMap<>();
+    private Map<String, byte[]> xslCache = new HashMap<>();
 
     public CustomUriResolver() throws IOException {
         String path = "src/examples/mapplanv01/SchemaCommon";
@@ -27,11 +30,35 @@ public class CustomUriResolver implements URIResolver {
                 cache.put(file.getName().toLowerCase().trim(), bytes);
             }
         }
+        cacheXsl(new File(basePathForXsl));
+    }
+
+    private void cacheXsl(File root) throws IOException {
+        if (root.isDirectory()) {
+            for (File file : root.listFiles()) {
+                if (file.isDirectory()) {
+                    cacheXsl(file);
+                } else {
+
+                    InputStream stream = loader.getResourceAsStream(file.getPath().replace("src/",""));
+                    byte[] bytes = IOUtils.readAllBytes(stream);
+                    xslCache.put(file.getName().toLowerCase().trim(), bytes);
+                }
+            }
+        }
     }
 
     private Source getStreamSourceFromCache(String fileName) {
         if (cache.containsKey(fileName)) {
-            InputStream is =  new ByteArrayInputStream(cache.get(fileName));
+            InputStream is = new ByteArrayInputStream(cache.get(fileName));
+            return new StreamSource(is);
+        }
+        return null;
+    }
+
+    private Source getStreamSourceFromXslCache(String fileName) {
+        if (xslCache.containsKey(fileName)) {
+            InputStream is = new ByteArrayInputStream(xslCache.get(fileName));
             return new StreamSource(is);
         }
         return null;
@@ -40,8 +67,9 @@ public class CustomUriResolver implements URIResolver {
     @Override
     public Source resolve(String href, String base) throws TransformerException {
         if (href != null && href.endsWith(".xsl")) {
-            InputStream stream = loader.getResourceAsStream(basePathForXsl + href);
-            return new StreamSource(stream);
+            String[] split = href.split("/");
+            String hrefFileName = split[split.length - 1].toLowerCase().trim();
+            return getStreamSourceFromXslCache(hrefFileName);
         } else if (href != null && href.endsWith(".xsd")) {
             String[] split = href.split("/");
             String hrefFileName = split[split.length - 1].toLowerCase().trim();
